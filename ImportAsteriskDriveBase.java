e88cb83ea0bc376cdb3c034b7c5fafe311dda7b0
Alex Skillin <Skillinator@users.noreply.github.com>
2016-01-26 16:32:34 -0600

    Update ImportAsteriskDriveBase.java

--- ImportAsteriskDriveBase.java
+++ ImportAsteriskDriveBase.java
@@ -1,76 +1,20 @@
 package com.qualcomm.ftcrobotcontroller.opmodes;
 
-//------------------------------------------------------------------------------
-//
-// PushBotManual
-//
-/**
- * Extends the PushBotTelemetry and PushBotHardware classes to provide a basic
- * manual operational mode for the Push Bot.
- *
- * @author SSI Robotics
- * @version 2015-08-01-06-01
- */
+
 public class ImportAsteriskDriveBase extends ImportAsteriskTelemetry
 
 {
-    //--------------------------------------------------------------------------
-    //
-    // PushBotManual
-    //
-    //--------
-    // Constructs the class.
-    //
-    // The system calls this member when the class is instantiated.
-    //--------
+   
     public ImportAsteriskDriveBase ()
 
     {
-        //
-        // Initialize base classes.
-        //
-        // All via self-construction.
+       
+    } 
 
-        //
-        // Initialize class members.
-        //
-        // All via self-construction.
-
-    } // PushBotManual::PushBotManual
-
-    //--------------------------------------------------------------------------
-    //
-    // loop
-    //
-    //--------
-    // Initializes the class.
-    //
-    // The system calls this member repeatedly while the OpMode is running.
-    //--------
     @Override public void loop ()
 
     {
-        //----------------------------------------------------------------------
-        //
-        // DC Motors
-        //
-        // Obtain the current values of the joystick controllers.
-        //
-        // Note that x and y equal -1 when the joystick is pushed all of the way
-        // forward (i.e. away from the human holder's body).
-        //
-        // The clip method guarantees the value never exceeds the range +-1.
-        //
-        // The DC motors are scaled to make it easier to control them at slower
-        // speeds.
-        //
-        // The setPower methods write the motor power values to the DcMotor
-        // class, but the power levels aren't applied until this method ends.
-        //
-
-        //
-        // Manage the drive wheel motors.
-        //
+        
         float l_gp1_left_stick_y = -gamepad1.left_stick_y;
         float l_left_drive_power
                 = (float)scale_motor_power (l_gp1_left_stick_y);
@@ -88,6 +32,7 @@ public class ImportAsteriskDriveBase extends ImportAsteriskTelemetry
         }
 
         v_motor_bulldozer.setPower(-gamepad2.right_stick_y/2);
+        
         //
         // Send telemetry data to the driver station.
         //
@@ -97,6 +42,6 @@ public class ImportAsteriskDriveBase extends ImportAsteriskTelemetry
         telemetry.addData ("13", "GP2 X: " + gamepad2.x);
         telemetry.addData ("14", "GP2 Y: " + gamepad2.left_stick_y);
 
-    } // PushBotManual::loop
+    }
 
-} // PushBotManual
+}
