e0884c1b3e2e232f4183a3b2314c560870374a7e
Alex Skillin <Skillinator@users.noreply.github.com>
2016-01-26 16:12:39 -0600

    Create ImportAsteriskDriveBase.java

---
+++ ImportAsteriskDriveBase.java
@@ -0,0 +1,116 @@
+package com.qualcomm.ftcrobotcontroller.opmodes;
+
+//------------------------------------------------------------------------------
+//
+// PushBotManual
+//
+/**
+ * Extends the PushBotTelemetry and PushBotHardware classes to provide a basic
+ * manual operational mode for the Push Bot.
+ *
+ * @author SSI Robotics
+ * @version 2015-08-01-06-01
+ */
+public class ImportAsteriskDriveBase extends ImportAsteriskTelemetry
+
+{
+    //--------------------------------------------------------------------------
+    //
+    // PushBotManual
+    //
+    //--------
+    // Constructs the class.
+    //
+    // The system calls this member when the class is instantiated.
+    //--------
+    public ImportAsteriskDriveBase ()
+
+    {
+        //
+        // Initialize base classes.
+        //
+        // All via self-construction.
+
+        //
+        // Initialize class members.
+        //
+        // All via self-construction.
+
+    } // PushBotManual::PushBotManual
+
+    //--------------------------------------------------------------------------
+    //
+    // loop
+    //
+    //--------
+    // Initializes the class.
+    //
+    // The system calls this member repeatedly while the OpMode is running.
+    //--------
+    @Override public void loop ()
+
+    {
+        //----------------------------------------------------------------------
+        //
+        // DC Motors
+        //
+        // Obtain the current values of the joystick controllers.
+        //
+        // Note that x and y equal -1 when the joystick is pushed all of the way
+        // forward (i.e. away from the human holder's body).
+        //
+        // The clip method guarantees the value never exceeds the range +-1.
+        //
+        // The DC motors are scaled to make it easier to control them at slower
+        // speeds.
+        //
+        // The setPower methods write the motor power values to the DcMotor
+        // class, but the power levels aren't applied until this method ends.
+        //
+
+        //
+        // Manage the drive wheel motors.
+        //
+        float l_gp1_left_stick_y = -gamepad1.left_stick_y;
+        float l_left_drive_power
+                = (float)scale_motor_power (l_gp1_left_stick_y);
+
+        float l_gp1_right_stick_y = -gamepad1.right_stick_y;
+        float l_right_drive_power
+                = (float)scale_motor_power (l_gp1_right_stick_y);
+
+        set_drive_power (l_left_drive_power, l_right_drive_power);
+
+        if(gamepad2.left_bumper)
+            triggerServo.setPosition(1.0);
+        if(gamepad2.right_bumper)
+            triggerServo.setPosition(0.0);
+        if(gamepad2.left_bumper && gamepad2.right_bumper)
+            triggerServo.setPosition(0.5);
+
+        if(gamepad2.left_trigger > 0.5){
+            climberServo.setPosition(1.0);
+        }else{
+            climberServo.setPosition(0.0);
+        }
+
+        if(gamepad2.right_trigger > 0.5){
+            armServo.setPosition(0);
+        }else{
+            armServo.setPosition(1.0);
+        }
+
+        v_motor_autoArm.setPower(-gamepad2.left_stick_y/2);
+        v_motor_winch.setPower(-gamepad2.right_stick_y/2);
+        //
+        // Send telemetry data to the driver station.
+        //
+        update_telemetry (); // Update common telemetry
+        telemetry.addData ("10", "GP1 Left: " + l_gp1_left_stick_y);
+        telemetry.addData ("11", "GP1 Right: " + l_gp1_right_stick_y);
+        telemetry.addData ("13", "GP2 X: " + gamepad2.x);
+        telemetry.addData ("14", "GP2 Y: " + gamepad2.left_stick_y);
+
+    } // PushBotManual::loop
+
+} // PushBotManual
